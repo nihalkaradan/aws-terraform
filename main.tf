a1b3f368e4a6a5bf4ebc57077b9889493f56e7f2
Nihal Karadan <nihalkaradan@gmail.com>
2021-05-31 14:13:33 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws"{
+  profile = "nihal-terraform"
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "Main"
+  }
+}
