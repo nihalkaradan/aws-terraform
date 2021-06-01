b87450842f676bcbd686519981105f66bb475dfb
Nihal Karadan <nihalkaradan@gmail.com>
2021-06-01 06:24:03 +0530

    public_subnet

--- main.tf
+++ main.tf
@@ -8,21 +8,43 @@ module "vpc" {
   Name = "main"
   source = "./vpc_module"
 }
+# create internet gateway
+resource "aws_internet_gateway" "ig" {
+  vpc_id = module.vpc.id
+  tags = {
+    "Name" = "ig"
+  }
+}
+# Routing table for public subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = module.vpc.id
+  tags = {
+    "Name" = "public-route-table"
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public_rt.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+}
 # Two public subnets and two private subnets 
 # Two AZ ap-south-1a and ap-south-1b
 module "public_subnet_A" {
   vpc_id     = module.vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
+  route_table_id = aws_route_table.public_rt.id
   Name = "Public A"
-  source = "./subnet_module"
+  source = "./public_subnet_module"
 }
 module "public_subnet_B" {
   vpc_id     = module.vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
+  route_table_id = aws_route_table.public_rt.id
   Name = "Public B"
-  source = "./subnet_module"
+  source = "./public_subnet_module"
 }
 module "private_subnet_A" {
   vpc_id     = module.vpc.id
@@ -37,12 +59,5 @@ module "private_subnet_B" {
   availability_zone = "ap-south-1b"
   Name = "Private B"
   source = "./subnet_module"
-}
-# add internet gateway to public subnets
-resource "aws_internet_gateway" "ig" {
-  vpc_id = module.vpc.id
-}
-resource "aws_route" "name" {
-  
 }
 
--- public_subnet_module/public_subnet_module.tf
+++ public_subnet_module/public_subnet_module.tf
@@ -2,7 +2,7 @@ variable "vpc_id" {}
 variable "cidr_block" {}
 variable "availability_zone" {}
 variable "Name" {}
-
+variable "route_table_id" {}
 resource "aws_subnet" "subnet" {
   vpc_id     = var.vpc_id
   cidr_block = var.cidr_block
@@ -11,7 +11,7 @@ resource "aws_subnet" "subnet" {
     Name = var.Name 
   }
 }
-
-output "id" {
-  value = aws_subnet.subnet.id
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.subnet.id
+  route_table_id = var.route_table_id
 }
